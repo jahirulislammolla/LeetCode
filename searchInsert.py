cdedd375ed322a90625aa6573c6a1e8705c9ff1a
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-01-28 21:29:14 +0600

    Update searchInsert.py

--- searchInsert.py
+++ searchInsert.py
@@ -10,3 +10,28 @@ class Solution(object):
         if x==[]:
             return l
         return x[0]
+  #second way.............
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # l=len(nums)
+        # x=[i for i in range(l) if nums[i]>=target]
+        # if x==[]:
+        #     return l
+        # return x[0]
+        x=len(nums)
+        s,e=0,x
+        #print(x)
+        while s<e:
+            x=(s+e)//2
+            if  nums[x]<target:
+                s=x+1
+            if nums[x]>target:
+                e=x
+            if nums[x]==target:
+                return x
+        return s
