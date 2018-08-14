48c1fbdc19bedba3598bf2e4079dc41a367baca9
Jahirul Islam <jahirul.iit5th@gmail.com>
2018-08-14 21:25:35 +0600

    Update deleteDuplicates.py

--- deleteDuplicates.py
+++ deleteDuplicates.py
@@ -11,3 +11,26 @@ class Solution:
             curr=curr.next
         return head
             
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    var cur=head
+    while (cur)
+        {
+            while(cur.next && cur.val==cur.next.val)
+                {
+                    cur.next=cur.next.next;
+                }
+            cur=cur.next;
+        }
+    return head;
+};
